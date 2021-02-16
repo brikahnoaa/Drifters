f81f54e2198a3683ae7b8baee6269bb829db0fb7
Brian Kahn <brian.kahn@noaa.gov>
2021-02-16 11:11:29 -0800

    whereat.1 Gps.class

--- whereat.py
+++ whereat.py
@@ -1,79 +1,30 @@
 #!/usr/bin/python3
-from datetime import datetime
+# v2 Tue 02 Feb 2021 09:53:51 AM UTC
+import time
+import sys
 import serial
-from serial.tools.list_ports import comports
+from Gps import Gps 
+from importlib import reload
 
 # settings
 interval = 'minute'   # minute hour or day
 
 # which serial? first one
+from serial.tools.list_ports import comports
 port = comports()[0].device
 ser = serial.Serial(port, 19200, timeout=1)
+gps = Gps(ser)
 
-def lookFor(key, lin, dic, ter='\r'):
-  # look for key.word in lin.e using ter.minator, store in dic.tionary
-  if isinstance(lin, bytes):
-    lin = lin.decode()
-  if key in lin:
-    at1=lin.find('=')+1
-    at2=lin.find(ter, at1)
-    dic[key]=lin[at1:at2]
-    return at2
-  else:
-    return 0
+"""
 
-def getGPS(ser):
-  """ parse GPS responses into dict """
-  gps = dict()
-  ser.write(b'at+pt\r')
-  lines = ser.readlines()
-  for l in lines:
-    x = lookFor('Time', l, gps, ter='.')
-  ser.write(b'at+pd\r')
-  lines = ser.readlines()
-  for l in lines:
-    x = lookFor('Date', l, gps)
-  ser.write(b'at+pl\r')
-  lines = ser.readlines()
-  for l in lines:
-    x = lookFor('Lati', l, gps)
-    x = lookFor('Long', l, gps)
-    x = lookFor('Used', l, gps)
-  return gps
-
-# main
-def main():
-  """main loop"""
-  global ser
-  fName = ''
-  # day of year
-  interval="%j"
-  day=datetime.now().strftime(interval)
-  if f"{day}.log" != fName: 
-    fName = f"{day}.log"
+while True:
+  dtl = gps.getGPS()
+  dt=g['Date'].split('-')[0:2]
+  tm=g['Time'].split(':')[0:2]
+  if not dt or not tm: 
+    break
   with open(fName, "w") as outF:
-    gps = getGPS(ser)
-    o = f"{gps['Time']}, Lati:{gps['Lati']}, Long:{gps['Long']}\n"
+    # "{0}{1}{2}{3}.log".format(*(dt+tm))
+    o = f"{gps['Date']} {gps['Time']}, Lati:{gps['Lati']}, Long:{gps['Long']}\n"
     outF.write(o)
-
-#main()
-
->>> dt=g['Date'].split('-')[0:2]
->>> tm=g['Time'].split(':')[0:2]
->>> tm
-['19', '57']
->>> "{0:02}{1:02}_{2:02}{3:02}".format(*(dt+tm))
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-  ValueError: '=' alignment not allowed in string format specifier
-  >>> dt
-  ['01', '13']
-  >>> "{0:02d}{1:02d}_{2:02d}{3:02d}".format(*(dt+tm))
-  Traceback (most recent call last):
-    File "<stdin>", line 1, in <module>
-    ValueError: Unknown format code 'd' for object of type 'str'
-    >>> "{0}{1}_{2}{3}".format(*(dt+tm))
-    '0113_1957'
-    >>> "{0}{1}{2}{3}.log".format(*(dt+tm))
-    '01131957.log'
-
+  """
