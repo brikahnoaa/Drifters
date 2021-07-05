75f576ca3cd893b98360922ded4e89543e0c32c9
Brian Kahn <brian.kahn@noaa.gov>
2021-07-05 16:21:39 -0700

    whereat release 1.0

--- code/whereat.py
+++ code/whereat.py
@@ -2,16 +2,26 @@
 # v2 Tue 02 Feb 2021 09:53:51 AM UTC
 import time
 import sys
+import os
 import serial
 from A3la import A3la 
+from Gps import Gps
 from pprint import pprint
 from importlib import reload
-
+#
+with open("pid", 'w') as pid:
+  pid.write(str(os.getpid()))
+#
 # settings
-
+#
 # which serial? first one
-from serial.tools.list_ports import comports
-port = comports()[0].device
+port = serial.tools.list_ports.comports()[0].device
 ser = serial.Serial(port, 19200, timeout=1)
 a3la = A3la(ser)
-
+gps = Gps(a3la,"gps.log")
+# delete pid file to stop loop
+while(os.path.isfile("pid")):
+  time.sleep(120)
+  data=gps.query()
+  with open("data",'a') as data
+    pprint(data,stream=datafile)
