8150b841febd45a3e59a8b515df7b9bd0e79be8b
Brian Kahn <brian.kahn@noaa.gov>
2021-08-22 19:45:43 -0700

    every 10 minutes

--- code/phonehome.py
+++ code/phonehome.py
@@ -11,17 +11,18 @@ import Rud
 from pprint import pprint,pformat
 from importlib import reload
 
-interval = 120
+interval = 600
 
 with open("pid", 'w') as pid:
   pid.write(str(os.getpid())+'\n')
 # which serial? first one
 port = serial.tools.list_ports.comports()[0].device
-buoyname = os.uname().nodename
 ser = serial.Serial(port, 19200, timeout=1)
 a3la = A3la.A3la(ser)
 gps = Gps.Gps(a3la)
 rud = Rud.Rud(a3la)
+buoyname = os.uname().nodename
+print(buoyname)
 # delete pid file to stop loop
 while(os.path.isfile("pid")):
   data=gps.query()
