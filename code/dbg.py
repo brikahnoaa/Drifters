cfb8cb91c1660f71102d4378e21b8e3acc051777
Brian Kahn <brian.kahn@noaa.gov>
2021-10-10 22:21:28 -0700

    added  buoy1 44.62097N,124.04421W  to message

--- code/dbg.py
+++ code/dbg.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
+# v2 Tue 02 Feb 2021 09:53:51 AM UTC
 import time
 import sys
 import os
 import serial
 import serial.tools.list_ports
-import A3la 
+import A3la
 import Gps
 import Rud
 from pprint import pprint,pformat
 from importlib import reload
-import code
-
-interval = 120
-
-port = serial.tools.list_ports.comports()[0].device
+interval = 600
+retry=9
+# which serial? first one
 buoy = os.uname().nodename
+port = serial.tools.list_ports.comports()[0].device
 ser = serial.Serial(port, 19200, timeout=1)
 a3la = A3la.A3la(ser)
 gps = Gps.Gps(a3la)
 rud = Rud.Rud(a3la, buoy)
+print(buoy)
+#
 print('data=gps.query()')
+import code
 code.interact(local=locals())
--- code/phonehome.py
+++ code/phonehome.py
@@ -10,12 +10,15 @@ import Gps
 import Rud
 from pprint import pprint,pformat
 from importlib import reload
-
+#
+def translate(s):
+  """convert A3LA format lat/long to generic
+     '44:37.2583 North' to '44.62080N' """
+  parts=s.replace(':',' ').split()
+  degree=float(parts[0])+float(parts[1])/60
+  return "{:.5f}{}".format(degree, parts[2][0])
 interval = 600
 retry=9
-
-with open("pid", 'w') as pid:
-  pid.write(str(os.getpid())+'\n')
 # which serial? first one
 buoy = os.uname().nodename
 port = serial.tools.list_ports.comports()[0].device
@@ -24,7 +27,11 @@ a3la = A3la.A3la(ser)
 gps = Gps.Gps(a3la)
 rud = Rud.Rud(a3la, buoy)
 print(buoy)
+#
+with open("pid", 'w') as pid:
+  pid.write(str(os.getpid())+'\n')
 # delete pid file to stop loop
+#
 while(os.path.isfile("pid")):
   data=gps.query()
   # make two datafiles, may be corrupted
@@ -32,11 +39,15 @@ while(os.path.isfile("pid")):
     pprint(data,stream=datafile)
   with open("data2",'a') as datafile:
     pprint(data,stream=datafile)
+  # translate 
+  lt=translate(data['Lati'])
+  ln=translate(data['Long'])
+  message='  {} {},{}\r{}'.format(buoy, lt, ln, pformat(data))
   #retry on failure(recursive)
   attempt=0
   success=False
   while not success:
-    success = rud.call(buoy+'\r'+pformat(data)+'\r')
+    success = rud.call(message)
     if success: break
     attempt+=1
     if attempt>=retry:
